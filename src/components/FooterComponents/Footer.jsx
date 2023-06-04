085801788fc33a308f08b36f921b618142f85da4
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2023-06-04 17:16:48 +0200

    React Router Installed

--- src/components/FooterComponents/Footer.jsx
+++ src/components/FooterComponents/Footer.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import "../../styles/FooterComponents/Footer.css";
 import Chocolate from "../../assets/backgrounds/ChocolateP.png";
-
+import InstagramIcon from "../../assets/icons/Instagram.png";
+import TelegramIcon from "../../assets/icons/telegram.png";
 const Footer = () => {
   return (
     <div className="FooterContainer">
       <div className="ChocolateFlex">
-        <img src={Chocolate} alt="" />
+        <img className="chocolateGlass" src={Chocolate} alt="" />
       </div>
       <div className="FooterFlex">
         <div>
           <p>LOGO</p>
-          <p>Copyright</p>
+          <p className="copyright">Copyright</p>
         </div>
 
         <div>
           <p>Contatti:</p>
           <div className="Social">
-            <a href="">Instagram</a>
-            <a href="">Telegram</a>
+            <a href="">
+              Instagram <img className="icon" src={InstagramIcon} alt="" />
+            </a>
+            <a href="">
+              Telegram <img className="icon" src={TelegramIcon} alt="" />
+            </a>
           </div>
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<App />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
