03376241af2671a991772decfc6584b737816fd2
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2023-06-04 16:19:42 +0200

    Footer

--- src/components/FooterComponents/Footer.jsx
+++ src/components/FooterComponents/Footer.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import "../../styles/FooterComponents/Footer.css";
+import Chocolate from "../../assets/backgrounds/ChocolateP.png";
 
 const Footer = () => {
   return (
-    <div>
-      <p>ciaoooooo</p>
+    <div className="FooterContainer">
+      <div className="ChocolateFlex">
+        <img src={Chocolate} alt="" />
+      </div>
+      <div className="FooterFlex">
+        <div>
+          <p>LOGO</p>
+          <p>Copyright</p>
+        </div>
+
+        <div>
+          <p>Contatti:</p>
+          <div className="Social">
+            <a href="">Instagram</a>
+            <a href="">Telegram</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/PageComponents/ProductCards.jsx
+++ src/components/PageComponents/ProductCards.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "../../styles/PageContainerStyle/PageContainer.css";
-
+import ChocolateP3 from "../../assets/backgrounds/ChocolateP3.png";
 export const ProductCards = (props) => {
   return (
     <div className="singleCard">
-      <img src={props.image} alt="" />
+      <div className="ChocolatePhoto">
+        <img src={ChocolateP3} alt="" />
+      </div>
+
+      <div className="ProductPhotoContainer">
+        <img className="ProductPhoto" src={props.image} alt="" />
+      </div>
+
       <p className="cardName">{props.title}</p>
     </div>
   );
