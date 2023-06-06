afb862154dece7722cc82d7fe946a6f512ace692
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2023-06-06 21:44:34 +0200

    fix

--- src/components/ContactComponents/TransparentContainer.jsx
+++ src/components/ContactComponents/TransparentContainer.jsx
@@ -2,28 +2,34 @@ import React from "react";
 import "../../styles/ContattiStyle/ContattiCSS.css";
 import telegramIcon from "../../assets/icons/telegram.png";
 import instagramIcon from "../../assets/icons/instagram.png";
-
+import ChocolateP from "../../assets/backgrounds/ChocolateP.png";
 const TransparentContainer = () => {
   return (
-    <div className="transparent-container">
-      <p>Prenota qui la tua commissione!</p>
-
-      <div className="ContactsWrapper">
-        <a href="https://t.me/jeandanchousama">
-          <div className="social-block" id="telegram-block">
-            <p>Telegram</p>
-            <img id="telegramIcon" src={telegramIcon} alt="" />
-          </div>
-        </a>
-
-        <a href="https://www.instagram.com/purinhandmade/">
-          <div className="social-block" id="instagram-block">
-            <p>Instagram</p>
-            <img id="instagramIcon" src={instagramIcon} alt="" />
-          </div>
-        </a>
+    <>
+      <div className="ChocolatePhoto">
+        <img src={ChocolateP} alt="" />
+        <img src={ChocolateP} alt="" />
       </div>
-    </div>
+      <div className="transparent-container">
+        <p>Prenota qui la tua commissione!</p>
+
+        <div className="ContactsWrapper">
+          <a href="https://t.me/jeandanchousama">
+            <div className="social-block" id="telegram-block">
+              <p>Telegram</p>
+              <img id="telegramIcon" src={telegramIcon} alt="" />
+            </div>
+          </a>
+
+          <a href="https://www.instagram.com/purinhandmade/">
+            <div className="social-block" id="instagram-block">
+              <p>Instagram</p>
+              <img id="instagramIcon" src={instagramIcon} alt="" />
+            </div>
+          </a>
+        </div>
+      </div>
+    </>
   );
 };
 
